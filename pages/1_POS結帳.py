6b68776ddc434b464940f2b2b13ad3fd9e47cff8
yujui <ray12150606@gmail.com>
2026-06-18 18:22:29 +0800

    feat: Comment out login check logic to streamline user access flow

--- pages/1_POS結帳.py
+++ pages/1_POS結帳.py
@@ -9,9 +9,9 @@ from database.db_core import log_history, deduct_stock_fifo, get_next_dish_id, u
 import streamlit as st
 
 # 檢查 session_state 中的登入狀態，若未登入則阻斷畫面並提示
-if not st.session_state.get("password_correct", False):
-    st.warning("🔒 請先前往首頁登入管理系統！")
-    st.stop()
+# if not st.session_state.get("password_correct", False):
+#     st.warning("🔒 請先前往首頁登入管理系統！")
+#     st.stop()
 show_pending_toast()
 
 st.subheader("🛒 收銀結帳與出餐管理系統")
